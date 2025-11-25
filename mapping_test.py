f42d5039568ac1acc8fee4ad60e05ef2e11f9996
tjuillac-cmd <tjuillac@gmail.com>
2025-11-25 12:04:40 +0100

    Modification du check

--- mapping_test.py
+++ mapping_test.py
@@ -1,17 +1,4 @@
 import sys,os
-flag=99
-
-# flagB = bin(int(flag)) # Transform the integer into a binary.
-# flagB = flagB[2:] # Remove '0b' Example: '0b1001101' > '1001101'
-# flagB = list(flagB) 
-# if len(flagB) < 12: # Size adjustement to 12 (maximal flag size)
-#     add = 12 - len(flagB) # We compute the difference between the maximal flag size (12) and the length of the binary flag.
-#     for t in range(add):
-#         flagB.insert(0,'0') # We insert 0 to complete until the maximal flag size.
-# print(flagB)
-
-
-
 
 def check(input_file):
     ''' Check if the input file exists and is in .sam format '''
@@ -35,22 +22,3 @@ def check(input_file):
                     print(f"Erreur de format à la ligne {line_index}: moins de 11 colonnes.")
                     sys.exit(1)
 
-
-
-
-    if len(sys.argv) != 2:
-        print("L'entrée doit être: python3 SamReader_template.py <sam_file>")
-        sys.exit(1)
-    elif sys.argv[1].endswith(".sam") == False:
-        print("Le fichier doit être au format .sam")
-        sys.exit(1)
-    else:
-        print("Check OK")
-
-
-
-
-sam_file = sys.argv[1]
-mapping = open(sam_file, "r")
-lines=mapping.readlines() #lecture de toutes les lignes du fichier
-print(lines[0:5])
