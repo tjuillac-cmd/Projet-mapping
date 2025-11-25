111e6395182718268c9a9faf3177925fe26ed76e
tjuillac-cmd <tjuillac@gmail.com>
2025-11-25 11:59:35 +0100

    Modification de la partie Check

--- mapping_test.py
+++ mapping_test.py
@@ -1,4 +1,4 @@
-import sys
+import sys,os
 flag=99
 
 # flagB = bin(int(flag)) # Transform the integer into a binary.
@@ -13,11 +13,34 @@ flag=99
 
 
 
-def check():
+def check(input_file):
+    ''' Check if the input file exists and is in .sam format '''
+    if not os.path.exists(input_file):
+        print("Aucun fichier trouvé")
+        sys.exit(1)
+    if not input_file.endswith(".sam"):
+        print("Le fichier doit être au format .sam")
+        sys.exit(1)
+    print("Exists and format check OK")
+
+    with open("input_file", "r") as file: #ouverture du fichier en lecture
+        for line, line_index in enumerate(file,start=1):
+            if line.startswith("@"):
+                if line.startswith("@SQ"):
+                    if "SN:" not in line or "LN:" not in line:
+                        print(f"Erreur de format à la ligne {line_index}: @SQ doit contenir SN: et LN:.")
+                        sys.exit(1)
+            else:
+                if line.split("\t") < 11:
+                    print(f"Erreur de format à la ligne {line_index}: moins de 11 colonnes.")
+                    sys.exit(1)
+
+
+
+
     if len(sys.argv) != 2:
         print("L'entrée doit être: python3 SamReader_template.py <sam_file>")
         sys.exit(1)
-    elif sys.argv[1] == "":
     elif sys.argv[1].endswith(".sam") == False:
         print("Le fichier doit être au format .sam")
         sys.exit(1)
