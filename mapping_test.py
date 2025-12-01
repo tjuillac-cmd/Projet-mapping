847da517bac096d4428211cac6b3f00904a4a0eb
tjuillac-cmd <tjuillac@gmail.com>
2025-12-01 23:56:22 +0100

    Modification du check

--- mapping_test.py
+++ mapping_test.py
@@ -1,14 +1,14 @@
-import sys,os
+import sys,os,re
 
 def check(input_file):
     '''
     Check if the input file exists and is in .sam format.
     Verifies that each alignment line (non-header) contains at least the 11 "mandatory" fields
     '''
-    if not os.path.exists(input_file):
+    if not os.path.exists(input_file): #check existence
         print("No file found")
         sys.exit(1)
-    if not input_file.endswith(".sam"):
+    if not input_file.endswith(".sam"): #check extension
         print("File must be in .sam format")
         sys.exit(1)
     print("Exists and format check OK")
@@ -38,22 +38,107 @@ def check(input_file):
                 print(f"Format error at line {line_index}: one of the 11 mandatory fields is empty.")
                 sys.exit(1)
 
-            # Check for mandatory numeric fields
-            for value, name in [(flag, "FLAG"), (pos, "POS"), (mapq, "MAPQ"), (pnext, "PNEXT"), (tlen, "TLEN")]:
-                try:
-                    int(value)
-                except ValueError:
-                    print(f"Format error at line {line_index}: {name} must be an interger.")
-                    sys.exit(1)
+            # Check if each field is in correct Regexp/Range
+            # QNAME
+            if not re.fullmatch(r'[!-?A-~]{1,254}', qname):
+                print(f"Format error at line {line_index}: QNAME incorrect.")
+                sys.exit(1)
+            
+            #FLAG: first check if of type integer then if in the right range
+            try:
+                int(flag)
+            except ValueError:
+                print(f"Format error at line {line_index}: FLAG must be an interger.")
+                sys.exit(1)
+            
+            if not (0 <= int(flag) <= 2**16 - 1):
+                print(f"Format error at line {line_index}: FLAG incorrect.")
+                sys.exit(1)
+
+            #RNAME
+            if not re.fullmatch(r'[0-9A-Za-z!#$%&+./:;?@^_|~-][0-9A-Za-z!#$%&*+./:;=?@^_|~-]*', rname):
+                print(f"Format error at line {line_index}: RNAME incorrect.")
+                sys.exit(1)
+
+            #POS: first check if of type integer then if in the right range
+            try:
+                int(pos)
+            except ValueError:
+                print(f"Format error at line {line_index}: POS must be an interger.")
+                sys.exit(1)
+
+            if not (0 <= int(pos) <= 2**31 - 1):
+                print(f"Format error at line {line_index}: POS incorrect.")
+                sys.exit(1)
+
+            #MAPQ: first check if of type integer then if in the right range
+            try:
+                int(mapq)
+            except ValueError:
+                print(f"Format error at line {line_index}: MAPQ must be an interger.")
+                sys.exit(1)
+
+            if not (0 <= int(mapq) <= 2**8 - 1):
+                print(f"Format error at line {line_index}: MAPQ incorrect.")
+                sys.exit(1)
+
+            #CIGAR
+            if not re.fullmatch(r'\*|([0-9]+[MIDNSHPX=])+', cigar):
+                print(f"Format error at line {line_index}: CIGAR incorrect.")
+                sys.exit(1)
+
+            #RNEXT
+            if not re.fullmatch(r'(?:\*|=|[0-9A-Za-z!#$%&+./:;?@^_|~-][0-9A-Za-z!#$%&*+./:;=?@^_|~-]*)', rnext):
+                print(f"Format error at line {line_index}: RNEXT incorrect.")
+                sys.exit(1)
+
+            #PNEXT: first check if of type integer then if in the right range
+            try:
+                int(pnext)
+            except ValueError:
+                print(f"Format error at line {line_index}: PNEXT must be an interger.")
+                sys.exit(1)
+
+            if not (0 <= int(pnext) <= 2**31 - 1):
+                print(f"Format error at line {line_index}: pNEXT incorrect.")
+                sys.exit(1)
+
+            #TLEN: first check if of type integer then if in the right range
+            try:
+                int(tlen)
+            except ValueError:
+                print(f"Format error at line {line_index}: TLEN must be an interger.")
+                sys.exit(1)
+
+            if not (-2**31 + 1 <= int(tlen) <= 2**31 - 1):
+                print(f"Format error at line {line_index}: TLEN incorrect.")
+                sys.exit(1)
+
+            #SEQ
+            if not re.fullmatch(r'\*|[A-Za-z=.]+', seq):
+                print(f"Format error at line {line_index}: SEQ incorrect.")
+                sys.exit(1)   
+
+            #QUAL
+            if not re.fullmatch(r'[!-~]+', qual):
+                print(f"Format error at line {line_index}: QUAL incorrect.")
+                sys.exit(1)     
+
+
+
+            # # Check for mandatory numeric fields
+            # for value, name in [(flag, "FLAG"), (pos, "POS"), (mapq, "MAPQ"), (pnext, "PNEXT"), (tlen, "TLEN")]:
+            #     if type(value) != int:
+            #         print(f"Format error at line {line_index}: {name} must be an interger.")
+            #         sys.exit(1)
 
             # # Check for mandatory string fields
-            # for value, name in [(rname, "RNAME"), (seq, "SEQ")]:
-                
+            # for value, name in [(qname,"QNAME"),(rname, "RNAME"),(cigar,"CIGAR"),(rnext,"RNEXT"),(seq,"SEQ"),(qual,"QUAL")]:
+            #     if type(value) != str:
+            #         print(f"Format error at line {line_index}: {name} must be of type string.")
+            #         sys.exit(1)
 
-
-            
-
-
+            # # for value, name in [(rname, "RNAME"), (seq, "SEQ")]:
 
     print("Format check OK")
 
@@ -66,5 +151,5 @@ def main():
     check(input_file)
 
 
-#if __name__ == "__main__":
-#    main()
+if __name__ == "__main__":
+    main()
